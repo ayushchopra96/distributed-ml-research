c3d5ddab2c40107e66e7e9485f925d1930ff22da
Surya Kant Sahu <surya.oju@gmail.com>
2021-07-16 20:27:28 +0530

    updted remaining jobs

--- split_nn_ucb/run_expts_cifar.py
+++ split_nn_ucb/run_expts_cifar.py
@@ -1,44 +1,44 @@
 import multiprocessing as mp
 import os
 
-NUM_GPUS = 8
+NUM_GPUS = 4
 
 commands = [
-    # UCB-CS + Polling
-    '''
-    CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
-    --cifar \
-    --num_clients 10 \
-    --k 2 --discount 0.7 \
-    --poll_clients \
-    --use_ucb \
-    --batch_size 64 \
-    --epochs 200
-    ''',
+    # # UCB-CS + Polling
+    # '''
+    # CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
+    # --cifar \
+    # --num_clients 10 \
+    # --k 2 --discount 0.7 \
+    # --poll_clients \
+    # --use_ucb \
+    # --batch_size 64 \
+    # --epochs 200
+    # ''',
 
-    # UCB-CS + No Polling
-    '''
-    CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
-    --cifar \
-    --num_clients 10 \
-    --k 2 --discount 0.7 \
-    --use_ucb \
-    --batch_size 64 \
-    --epochs 200
-    ''',
+    # # UCB-CS + No Polling
+    # '''
+    # CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
+    # --cifar \
+    # --num_clients 10 \
+    # --k 2 --discount 0.7 \
+    # --use_ucb \
+    # --batch_size 64 \
+    # --epochs 200
+    # ''',
 
-    # UCB-CS + Polling + Interrupted
-    '''
-    CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
-    --cifar \
-    --num_clients 10 \
-    --k 2 --discount 0.7 \
-    --poll_clients \
-    --use_ucb \
-    --interrupted \
-    --batch_size 64 \
-    --epochs 200
-    ''',
+    # # UCB-CS + Polling + Interrupted
+    # '''
+    # CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
+    # --cifar \
+    # --num_clients 10 \
+    # --k 2 --discount 0.7 \
+    # --poll_clients \
+    # --use_ucb \
+    # --interrupted \
+    # --batch_size 64 \
+    # --epochs 200
+    # ''',
 
     # UCB-CS + No Polling + Interrupted
     '''
@@ -88,40 +88,40 @@ commands = [
     --epochs 200
     ''',
 
-    # UCB-CS + No Polling + Interrupted + constrastive
-    '''
-    CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
-    --cifar \
-    --num_clients 10 \
-    --k 2 --discount 0.7 \
-    --interrupted \
-    --use_ucb \
-    --batch_size 64 \
-    --epochs 200
-    ''',
+    # # UCB-CS + No Polling + Interrupted + constrastive
+    # '''
+    # CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
+    # --cifar \
+    # --num_clients 10 \
+    # --k 2 --discount 0.7 \
+    # --interrupted \
+    # --use_ucb \
+    # --batch_size 64 \
+    # --epochs 200
+    # ''',
 
-    # Interrupted only + no contrastive learning
-    '''
-    CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
-    --cifar \
-    --num_clients 10 \
-    --k 2 --discount 0.7 \
-    --interrupted \
-    --batch_size 64 \
-    --epochs 200
-    ''',
+    # # Interrupted only + no contrastive learning
+    # '''
+    # CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
+    # --cifar \
+    # --num_clients 10 \
+    # --k 2 --discount 0.7 \
+    # --interrupted \
+    # --batch_size 64 \
+    # --epochs 200
+    # ''',
 
-    # Interrupted only + contrastive learning
-    '''
-    CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
-    --cifar \
-    --num_clients 10 \
-    --k 2 --discount 0.7 \
-    --interrupted \
-    --use_contrastive \
-    --batch_size 64 \
-    --epochs 200
-    ''',
+    # # Interrupted only + contrastive learning
+    # '''
+    # CUDA_VISIBLE_DEVICES={} python3 loop_flipped_10.py \
+    # --cifar \
+    # --num_clients 10 \
+    # --k 2 --discount 0.7 \
+    # --interrupted \
+    # --use_contrastive \
+    # --batch_size 64 \
+    # --epochs 200
+    # ''',
 ]
 
 
